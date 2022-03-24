bdae77e6b17d4584428a4ae4df9279d617489547
zhaopan <2272795814@qq.com>
2022-03-24 22:33:40 +0800

    feat : 问题列表

--- models/problem.go
+++ models/problem.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"fmt"
 	"gorm.io/gorm"
 )
 
@@ -19,10 +18,7 @@ func (table *Problem) TableName() string {
 	return "problem"
 }
 
-func GetProblemList() {
-	data := make([]*Problem, 0)
-	DB.Find(&data)
-	for _, v := range data {
-		fmt.Printf("Problem ==> %v \n", v)
-	}
+func GetProblemList(keyword string) *gorm.DB {
+	return DB.Model(new(Problem)).
+		Where("title like ? OR content like ? ", "%"+keyword+"%", "%"+keyword+"%")
 }
