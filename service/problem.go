15f08666be60d5c3c403d826206198c21eb490e6
zhaopan <2272795814@qq.com>
2022-03-25 22:28:31 +0800

    Revert "feat : 问题列表"
    This reverts commit bdae77e6b17d4584428a4ae4df9279d617489547.

--- service/problem.go
+++ service/problem.go
@@ -1,45 +1,19 @@
 package service
 
 import (
-	"getcharzp.cn/define"
 	"getcharzp.cn/models"
 	"github.com/gin-gonic/gin"
-	"log"
 	"net/http"
-	"strconv"
 )
 
 // GetProblemList
 // @Tags 公共方法
 // @Summary 问题列表
-// @Param page query int false "page"
+// @Param page query int false "请输入当前页，默认第一页"
 // @Param size query int false "size"
-// @Param keyword query string false "keyword"
-// @Success 200 {string} json "{"code":"200","data":""}"
+// @Success 200 {string} json "{"code":"200","msg","","data":""}"
 // @Router /problem-list [get]
 func GetProblemList(c *gin.Context) {
-	size, _ := strconv.Atoi(c.DefaultQuery("size", define.DefaultSize))
-	page, err := strconv.Atoi(c.DefaultQuery("page", define.DefaultPage))
-	if err != nil {
-		log.Println("GetProblemList Page strconv Error:", err)
-		return
-	}
-	page = (page - 1) * size
-	var count int64
-	keyword := c.Query("keyword")
-
-	list := make([]*models.Problem, 0)
-	tx := models.GetProblemList(keyword)
-	err = tx.Count(&count).Omit("content").Offset(page).Limit(size).Find(&list).Error
-	if err != nil {
-		log.Println("Get Problem List Error:", err)
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"code": 200,
-		"data": map[string]interface{}{
-			"list":  list,
-			"count": count,
-		},
-	})
+	models.GetProblemList()
+	c.String(http.StatusOK, "Get Problem List")
 }
