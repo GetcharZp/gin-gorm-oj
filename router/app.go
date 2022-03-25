8aac9f7ccc01078c5b9ce3e42ee68b50c3d1539f
zhaopan <2272795814@qq.com>
2022-03-25 21:46:36 +0800

    feat : 问题详情

--- router/app.go
+++ router/app.go
@@ -17,6 +17,7 @@ func Router() *gin.Engine {
 	// 路由规则
 	r.GET("/ping", service.Ping)
 	r.GET("/problem-list", service.GetProblemList)
+	r.GET("/problem-detail", service.GetProblemDetail)
 
 	return r
 }
--- service/problem.go
+++ service/problem.go
@@ -4,9 +4,11 @@ import (
 	"getcharzp.cn/define"
 	"getcharzp.cn/models"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 // GetProblemList
@@ -41,5 +43,53 @@ func GetProblemList(c *gin.Context) {
 			"list":  list,
 			"count": count,
 		},
+	})
+}
+
+// GetProblemDetail
+// @Tags 公共方法
+// @Summary 问题详情
+// @Param identity query string true "问题的唯一标识"
+// @Success 200 {string} json "{"code":"200","data":""}"
+// @Success 400 {string} json "{"code":"-1","msg":""}"
+// @Router /problem-detail [get]
+func GetProblemDetail(c *gin.Context) {
+	problemIdentity := c.Query("identity")
+	if problemIdentity == "" {
+		c.JSON(http.StatusOK, gin.H{
+			"code": -1,
+			"msg":  "问题标识必传",
+		})
+		return
+	}
+	data := new(models.Problem)
+	err := models.DB.Model(new(models.Problem)).Where("identity = ?", problemIdentity).First(&data).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusOK, gin.H{
+				"code": -1,
+				"msg":  "该问题不存在",
+			})
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"code": -1,
+			"msg":  "Gorm First models.Problem Error:" + err.Error(),
+		})
+		return
+	}
+	// 手动查询关联的分类信息
+	categories := make([]*models.Category, 0)
+	err = models.DB.Debug().Model(new(models.Category)).Where("id in ? ", strings.Split(data.CategoryId, ",")).Find(&categories).Error
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": -1,
+			"msg":  "Get Category Error:" + err.Error(),
+		})
+		return
+	}
+	data.Categories = categories
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"data": data,
 	})
 }
