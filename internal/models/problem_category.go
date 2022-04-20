fea8e1168f6e31adfb2073516f837be06cc143ef
zhaopan <2272795814@qq.com>
2022-04-20 23:11:50 +0800

    fix : 统一返回数据格式，下划线

--- internal/models/problem_category.go
+++ internal/models/problem_category.go
@@ -1,12 +1,18 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type ProblemCategory struct {
-	gorm.Model
-	ProblemId     uint           `gorm:"column:problem_id;type:varchar(36);" json:"problem_id"`   // 问题的ID
-	CategoryId    uint           `gorm:"column:category_id;type:varchar(36);" json:"category_id"` // 分类的ID
-	CategoryBasic *CategoryBasic `gorm:"foreignKey:id;references:category_id"`                    // 关联分类的基础信息表
+	ID            uint           `gorm:"primarykey;" json:"id"`
+	CreatedAt     time.Time      `json:"created_at"`
+	UpdatedAt     time.Time      `json:"updated_at"`
+	DeletedAt     gorm.DeletedAt `gorm:"index;" json:"deleted_at"`
+	ProblemId     uint           `gorm:"column:problem_id;type:varchar(36);" json:"problem_id"`       // 问题的ID
+	CategoryId    uint           `gorm:"column:category_id;type:varchar(36);" json:"category_id"`     // 分类的ID
+	CategoryBasic *CategoryBasic `gorm:"foreignKey:id;references:category_id;" json:"category_basic"` // 关联分类的基础信息表
 }
 
 func (table *ProblemCategory) TableName() string {
